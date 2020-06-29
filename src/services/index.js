8e5f2c2df2b056d45ec564d819c3326b813aec4b
mstjepan28 <55623249+mstjepan28@users.noreply.github.com>
2020-06-29 09:23:07 +0200

    Update index.js

--- src/services/index.js
+++ src/services/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import $router from '@/router'
 
 let Service = axios.create({
-    baseURL: "http://localhost:5000/"
+    baseURL: "https://digitalityapp.herokuapp.com/"
 });
 
 
